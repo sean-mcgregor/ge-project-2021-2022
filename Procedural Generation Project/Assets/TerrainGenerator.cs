7d408334a1f4a5179776cb71bdb7cbf9b60c5404
sean-mcgregor <worksmcg@gmail.com>
2021-11-19 18:04:29 +0000

    Added textures to terrain and water

--- Procedural Generation Project/Assets/TerrainGenerator.cs
+++ Procedural Generation Project/Assets/TerrainGenerator.cs
@@ -11,31 +11,44 @@ public class TerrainGenerator : MonoBehaviour {
     public float terrain_offset_x =  10f;
     public float terrain_offset_y =  10f;
 
+    public int counter = 0;
+
+    void Start() {
+        
+        generateOffset();
+    }
+
     void Update(){
 
-        generateOffset();
         Terrain terrain =  GetComponent<Terrain>();
         terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        terrain_offset_x = terrain_offset_x + 0.01f;
     }
 
     void generateOffset() {
 
-        terrain_offset_x = Random.Range(0f, 100f);
-        terrain_offset_y = Random.Range(0f, 100f);
-
-
+        terrain_offset_x = Random.Range(0f, 50f);
+        terrain_offset_y = Random.Range(0f, 200f);
     }
 
     // Build a 2D array of heights
     float[,] GenerateHeights(){
 
+        int yToSendValue = 0;
         float[,] heights = new float[terrain_width, terrain_height];
         
         for (int x = 0; x < terrain_width; x++) {
 
             for (int y = 0; y < terrain_height; y++) {
 
-                heights[x,y] = CalculateHeights(x,y);
+                yToSendValue = y;
+
+                if (x < 256){
+
+                    yToSendValue = (y + 200);
+                }
+
+                heights[x,y] = CalculateHeights(x,yToSendValue);
             }
 
         }
