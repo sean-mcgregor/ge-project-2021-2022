a38a4da0d93dfe12fee18e2ca5172b534e870da7
sean-mcgregor <worksmcg@gmail.com>
2021-12-09 17:50:09 +0000

    Fixed rotation of boat model to face the same way that train was before destroyed

--- Procedural Generation Project/Assets/ChangeCharacter.cs
+++ Procedural Generation Project/Assets/ChangeCharacter.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ChangeCharacter : MonoBehaviour
 {
-    public bool PlayerDead;//Determine PlayerDead in your health script
+    public bool onWater; //Determine if player on water
     private GameObject currentModel;
-    public GameObject FirstPlayerModelPrefab;//Drag the initial player prefab in here
-    public GameObject SecondPlayerModelPrefab;//Drag the second player prefab in here
+    public GameObject FirstPlayerModelPrefab; //Drag the initial player prefab in here
+    public GameObject SecondPlayerModelPrefab; //Drag the second player prefab in here
     public GameObject Player;
 
-    private bool wasDeadLastFrame = false;
+    private bool onWaterLastFrame = false;
 
     private void Start() {
         
@@ -20,13 +20,14 @@ public class ChangeCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PlayerDead && !wasDeadLastFrame)
+        if (onWater && !onWaterLastFrame)
         {
+            Vector3 rotation = new Vector3(0,Player.transform.localRotation.eulerAngles.y, 0);
             Destroy(currentModel);
-            currentModel = Instantiate(SecondPlayerModelPrefab, Player.transform.position, Quaternion.identity);
+            currentModel = Instantiate(SecondPlayerModelPrefab, Player.transform.position, Quaternion.Euler(rotation));
             currentModel.transform.SetParent(Player.transform);
         }
 
-        wasDeadLastFrame = PlayerDead;
+        onWaterLastFrame = onWater;
     }
 }
