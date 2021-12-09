b55b5a5669c19fd0b01d1cfad1dcf2b2b5e4f171
sean-mcgregor <worksmcg@gmail.com>
2021-12-09 17:02:00 +0000

    Change from train to boat now fully functional when player marked as dead manually

--- Procedural Generation Project/Assets/ChangeCharacter.cs
+++ Procedural Generation Project/Assets/ChangeCharacter.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class ChangeCharacter : MonoBehaviour
 {
     public bool PlayerDead;//Determine PlayerDead in your health script
-    private GameObject currentPlayer;
+    private GameObject currentModel;
     public GameObject FirstPlayerModelPrefab;//Drag the initial player prefab in here
     public GameObject SecondPlayerModelPrefab;//Drag the second player prefab in here
-
     public GameObject Player;
 
     private bool wasDeadLastFrame = false;
+
+    private void Start() {
+        
+        currentModel = Player.transform.GetChild(0).gameObject;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (PlayerDead && !wasDeadLastFrame)
         {
-            Destroy(FirstPlayerModelPrefab);
-            currentPlayer = Instantiate(SecondPlayerModelPrefab, Player.transform.position, Quaternion.identity);
-            currentPlayer.transform.SetParent(Player.transform);
+            Destroy(currentModel);
+            currentModel = Instantiate(SecondPlayerModelPrefab, Player.transform.position, Quaternion.identity);
+            currentModel.transform.SetParent(Player.transform);
         }
 
         wasDeadLastFrame = PlayerDead;
