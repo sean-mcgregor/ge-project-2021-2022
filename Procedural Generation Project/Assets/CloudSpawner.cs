58b5c14b8c1643c53dcf5103a0580f613f5bc599
sean-mcgregor <worksmcg@gmail.com>
2021-12-12 19:16:54 +0000

    Optimised memory usage by deleting clouds instead of them existing permanently

--- Procedural Generation Project/Assets/CloudSpawner.cs
+++ Procedural Generation Project/Assets/CloudSpawner.cs
@@ -8,18 +8,32 @@ public class CloudSpawner : MonoBehaviour
     public float spawnTime;
     public float spawnDelay;
     public bool stopSpawning;
+    public GameObject[] activeClouds;
+
+    public int maxClouds = 6;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating ("SpawnObject", spawnTime, spawnDelay);
     }
+
+    void Update() {
+        
+        activeClouds = GameObject.FindGameObjectsWithTag("Cloud");
+
+        if(activeClouds.Length > maxClouds){
+
+            Destroy(activeClouds[0]);
+        }
+    }
     
 
     // Update is called once per frame
     public void SpawnObject()
     {
-        Instantiate (cloud, transform.position, transform.rotation);
+        GameObject newCloud = Instantiate (cloud, transform.position, transform.rotation);
+        newCloud.tag = "Cloud";
 
         if (stopSpawning)
         {
